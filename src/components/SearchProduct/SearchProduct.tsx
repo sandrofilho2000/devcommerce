81e7635d98a4981c48c7b89814decd1a7df3f89d
devgustavovasquez <gustavovasquez2002@gmail.com>
2022-06-28 12:54:45 -0300

    feat: create request products in Home page

--- src/components/SearchProduct/SearchProduct.tsx
+++ src/components/SearchProduct/SearchProduct.tsx
@@ -9,12 +9,12 @@ export const SearchProduct: React.FC<Props> = () => {
       <input
         placeholder="Pesquisar produto..."
         type="text"
-        id="search"
         className="invalid:w-0 h-6 bg-transparent border-transparent transition-all duration-500 outline-none focus:w-40 group-hover:w-40 valid:w-40 sm:group-hover:w-60 sm:focus:w-60 sm:valid:w-60 font-normal"
         autoComplete="off"
         required
       />
       <MagnifyingGlass weight="bold" className="w-6 h-6" />
+
     </label>
   );
 };
