58c21e0f6bec326f62b42e72bc12f573b4a0bfac
devgustavovasquez <gustavovasquez2002@gmail.com>
2022-06-28 12:53:19 -0300

    feat: create request products in Home page

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export * from './SearchProduct/SearchProduct';
 export * from './ProductCard/ProductCard';
 export * from './ProductsContainer/ProductsContainer';
 export * from './MainContainer/MainContainer';
+export * from './Modal/Modal';
--- src/pages/Base/Base.tsx
+++ src/pages/Base/Base.tsx
@@ -1,14 +1,20 @@
-import React, { ReactNode } from 'react';
+import React, { Dispatch, ReactNode, SetStateAction } from 'react';
 import { Header, MainContainer } from '../../components';
 
 type Props = {
   children: ReactNode;
+  setModal: () => void;
+  handleHasAccount: (state: boolean) => void;
 };
 
-export const Base: React.FC<Props> = ({ children }) => {
+export const Base: React.FC<Props> = ({
+  children,
+  setModal,
+  handleHasAccount,
+}) => {
   return (
     <>
-      <Header />
+      <Header setModal={setModal} handleHasAccount={handleHasAccount} />
       <MainContainer>{children}</MainContainer>
     </>
   );
