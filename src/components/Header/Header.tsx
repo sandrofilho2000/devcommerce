33e59d336d1407f8900898ffc449eb2b2599a955
sandrofilho2000 <sandrofilho98@yahoo.com.br>
2022-06-28 17:12:21 -0300

    fix: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,8 @@ type Props = {};
 export const Header: React.FC<Props> = () => {
   return (
     <header className="max-h-22 py-2 flex justify-center bg-zinc-200">
-      <div className="max-w-7xl w-full relative flex justify-between items-center">
+      <div className="max-w-7xl w-11/12 relative flex justify-between items-center">
+
         <Link to="/pagina-inicial">
           <img className="max-w-xs w-8/12" src={LogoImg} alt="logo" />
         </Link>
