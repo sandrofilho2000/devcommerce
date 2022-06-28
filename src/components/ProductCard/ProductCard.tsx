005e150b2e6f567736828243d3f22cb0d7a2233d
sandrofilho2000 <sandrofilho98@yahoo.com.br>
2022-06-27 21:51:11 -0300

    fix: making product text more responsive

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export const ProductCard: React.FC<Props> = ({ title, price, image }) => {
   return (
-    <div className="relative w-full h-full  max-w-[16.5rem] max-h-[18rem]">
+    <div className="relative w-full h-full  max-w-[16.5rem] ">
       <button className="absolute top-3 right-3 border-solid border-2 rounded-md border-neutral-300 hover:bg-zinc-200 transition-colors focus:outline-none ring-0 focus:border-neutral-500 cursor-pointer">
         <Plus weight="bold" size="20px" />
       </button>
@@ -19,16 +19,16 @@ export const ProductCard: React.FC<Props> = ({ title, price, image }) => {
           <img src={image} alt={title} />
         </Link>
       </div>
-      <div className="p-1 flex flex-col gap-3">
+      <div className="p-1 flex flex-col">
         <Link to="">
-          <h2 className="text-lg text-neutral-500 tracking-wide">{title}</h2>
+          <h2 className="sm:text-base md:text-lg text-neutral-500 tracking-wide">{title}</h2>
         </Link>
 
         <div className="flex gap-4 items-center">
-          <p className="text-lg tracking-tighter">
+          <p className="text-base md:text-lg tracking-tighter">
             R$ {price.toFixed(2).replace('.', ',')}
           </p>
-          <p className="text-sm line-through tracking-tighter text-neutral-400">
+          <p className="sm:text-base md:text-lg line-through tracking-tighter text-neutral-400">
             R$ {(price + Math.random() * 10).toFixed(2).replace('.', ',')}
           </p>
         </div>
